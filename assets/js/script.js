0da360250364b0f1800d06e3d5e84dd41dd5cad3
Mazen Mohamed <kidkever@yahoo.com>
2021-10-13 07:11:32 +0200

    finishing homepage

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+$(".services-carousel").owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 30,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".our-clients-carousel").owlCarousel({
+  rtl: true,
+  loop: true,
+  margin: 30,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
